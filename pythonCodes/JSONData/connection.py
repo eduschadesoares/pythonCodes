da55746148a465e7b96cdca18164f334cae067fb
Eduardo <eduschadesoares@hotmail.com>
2018-06-30 02:19:56 -0300

    Connection

--- pythonCodes/JSONData/connection.py
+++ pythonCodes/JSONData/connection.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import requests
+
+localhost = '127.0.0.1'
+port = '9000'
+
+class Connection:
+    def __init__(self):
+        self.connection = self.estabilish_connection(localhost, port)
+
+    def estabilish_connection(self, localhost, port):
+        url = 'http://{}:{}/status'.format(localhost, port)
+        try:
+            connection = requests.get(url)
+            if connection:
+                print("Connected to server!")
+            return True
+        except Exception as error:
+            print("Could not connect to server!")
+            print(error)
+            return False
--- pythonCodes/JSONData/start.py
+++ pythonCodes/JSONData/start.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append('..')
+
+from connection import Connection
+
+if __name__ == '__main__':
+    connect = Connection()
+
+else:
+    pass
