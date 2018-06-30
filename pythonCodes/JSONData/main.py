9237cb4585366e24bf82281b91706c27c1f34c5c
Eduardo <eduschadesoares@hotmail.com>
2018-06-30 00:39:24 -0300

    get JSON localhost

---
+++ pythonCodes/JSONData/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+port = '8080'
+
+# params = {'localhost': localhost, 'port': port}
+
+session = 'musics'
+
+url = '{}:{}/{}'.format(localhost, port, session)
+
+print(data)
