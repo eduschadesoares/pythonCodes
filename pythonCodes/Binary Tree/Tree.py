7dbe94bbd4219c220a7c2703372d8142e63dd4e6
Eduardo <eduschadesoares@hotmail.com>
2018-07-09 14:51:25 -0300

    Binary Tree Python

---
+++ pythonCodes/Binary Tree/Tree.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Tree:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.counter = 1
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Tree(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Tree(data)
+                else:
+                    self.right.insert(data)
+            else:
+                self.counter += 1
+        else:
+            self.data = data
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data, self.counter)
+        if self.right:
+            self.right.printTree()
+
+
+
+root = Tree(12)
+root.insert(9)
+root.insert(12)
+root.printTree()
