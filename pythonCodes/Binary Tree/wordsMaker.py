e3b318968e9815925c8de203a39dce60105f97b3
Eduardo <eduschadesoares@hotmail.com>
2018-07-10 18:03:50 -0300

    Worder

--- pythonCodes/Binary Tree/wordsMaker.py
+++ pythonCodes/Binary Tree/wordsMaker.py
@@ -3,18 +3,33 @@
 
 import requests
 from bs4 import BeautifulSoup
+from fileHandler import File
+import re
 
 
 def connect():
-    url = 'https://www.pythoncentral.io/how-to-get-a-substring-from-a-string-in-python-slicing-strings/'
+    # url = 'https://www.pythoncentral.io/how-to-get-a-substring-from-a-string-in-python-slicing-strings/'
+    url = 'https://imasters.com.br/back-end/aprendendo-sobre-web-scraping-em-python-utilizando-beautifulsoup'
 
     r = requests.get(url)
 
-    soup = BeautifulSoup(r.content)
+    soup = BeautifulSoup(r.content, "html.parser")
 
-    p_words = soup.p
+    for i in range(50):
+        p_words = soup.find_all('p')[i].get_text()
 
-    for p in p_words:
-        print("aqui", p)
+        all_words = re.split('[. ,]', p_words)
+
+        #print(all_words)
+
+        file = File()
+        for i in all_words:
+            file.writeFile(i)
+            print(i)
+
+   # print(p_words)
+
+#     for p in p_words:
+#       print("aqui", p)
 
 connect()
