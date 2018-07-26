42990c0b80a2e80537266bf728d27ed59087cd85
Eduardo <eduschadesoares@hotmail.com>
2018-07-26 01:38:10 -0300

    Some classes

--- pythonCodes/Classes/Animals.py
+++ pythonCodes/Classes/Animals.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
 class Animal:
     def __init__(self, name, genre):
         self.name = name
         self.genre = genre
 
     def __str__(self):
-        return "{} {}" % (self.name + self.genre)
+        return "{} {}".format(self.name, self.genre)
+
 
 a = Animal("rodolfo", "masculino")
 
